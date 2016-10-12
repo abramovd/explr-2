3772cfb51249426b08b6949c588c834872da68d8
Dmitry Abramov <diabramo@ya.ru>
2016-10-12 23:23:25 +0300

    pinax old added:

---
+++ Pinax-0.9/pinax/apps/account/auth_backends.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+
+class AuthenticationBackend(ModelBackend):
+    
+    def authenticate(self, **credentials):
+        lookup_params = {}
+        if settings.ACCOUNT_EMAIL_AUTHENTICATION:
+            lookup_params["email"] = credentials["email"]
+        else:
+            lookup_params["username"] = credentials["username"]
+        try:
+            user = User.objects.get(**lookup_params)
+        except User.DoesNotExist:
+            return None
+        else:
+            if user.check_password(credentials["password"]):
+                return user
+    
+    def has_perm(self, user, perm):
+        # @@@ allow all users to add wiki pages
+        if perm in ["wakawaka.add_wikipage", "wakawaka.add_revision"]:
+            return True
+        return super(AuthenticationBackend, self).has_perm(user, perm)
+
+
+EmailModelBackend = AuthenticationBackend
